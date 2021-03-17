6d32bbad8e620665cf8f85948eef97cfa4f763a6
zaynegoering <zaynegoering@gmail.com>
2021-03-17 11:56:08 -0700

    define createRecipe action

---
+++ src/actions/recipeActions.js
@@ -0,0 +1,18 @@
+const url = 'http://localhost:3000/recipes'
+
+export const createToy = (recipe) => {
+    
+    const configObj = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accepts": "application/json"
+        },
+        body: JSON.stringify(recipe)
+    }
+    fetch(url, configObj)
+    .then(r => r.json())
+    .then(json =>
+        console.log(json)
+        )
+}
