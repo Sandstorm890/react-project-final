8b61febe1e0a8e56fc6e994d96e2248e303ea845
zaynegoering <zaynegoering@gmail.com>
2021-03-17 12:16:35 -0700

    define create and getRecipes functions

--- src/actions/recipeActions.js
+++ src/actions/recipeActions.js
@@ -1,18 +1,35 @@
 const url = 'http://localhost:3000/recipes'
 
+export const addRecipe = (recipe) => ({ type: 'ADDED_RECIPE', payload: recipe})
+
+export const setRecipes = (recipes) => ({type: "GOT_RECIPES", payload: recipes})
+
 export const createRecipe = (recipe) => {
-    
-    const configObj = {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            "Accepts": "application/json"
-        },
-        body: JSON.stringify(recipe)
+    return (dispatch) => {
+        const configObj = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Accepts": "application/json"
+            },
+            body: JSON.stringify(recipe)
+        }
+        fetch(url, configObj)
+        .then(r => r.json())
+        .then(json => {
+            dispatch(addRecipe(json))
+        })
     }
-    fetch(url, configObj)
-    .then(r => r.json())
-    .then(json =>
-        console.log(json)
-        )
+}
+
+export const getRecipes = () => {
+    return (dispatch) => {
+        dispatch({type: "LOADING"})
+        fetch(url)
+        .then(r => r.json())
+        .then(json => {
+            dispatch(setRecipes(json))
+        })
+    }
+        
 }
