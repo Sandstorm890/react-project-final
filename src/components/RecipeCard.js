936528e6e74c2e9b6ae4f1f6b09656d872785386
zaynegoering <zaynegoering@gmail.com>
2021-03-17 17:40:39 -0700

    move category to bottom of card

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,12 +1,12 @@
 
 
 function RecipeCard(props)  {
-    
+
     return(
-        <div className={'recipe-card-' + props.id}>
+        <div id={'recipe-card-' + props.id}>
             <h2>{props.recipe.name}</h2>
-            <h4>Category: {props.recipe.category.name}</h4>
             <img src={props.recipe.img_url} className="recipe-image img-thumbnail img-fluid" alt="None"/>
+            <h4>Category: {props.recipe.category.name}</h4>
             <p>{props.recipe.description}</p>
         </div>
     )
