49d347997e22273761f89ecde1811d02ec2876fe
zaynegoering <zaynegoering@gmail.com>
2021-03-22 10:35:04 -0700

    update data structure interpretation

--- src/reducers/recipeReducer.js
+++ src/reducers/recipeReducer.js
@@ -1,13 +1,13 @@
 const recipeReducer = (state = {recipes: []}, action) => {
     switch(action.type){
         case "GOT_RECIPES":
-            return {...state, recipes: action.payload}
+            return {...state, recipes: action.payload.data}
         case "ADDED_RECIPE":
-            return {...state, recipes: [...state.recipes.data, action.payload]}
+            return {...state, recipes: [...state.recipes, action.payload.data]}
         case "DELETE_RECIPE":
             if (state.recipes.data) {
                 return {recipes: state.recipes.data.filter(r => r !== action.payload)}
-            }
+            } else {break}
         default:
             return state
     }
