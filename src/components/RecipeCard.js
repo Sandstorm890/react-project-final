9c43bbfdf61dcad2bdf1c8391a82d7a9da74d8be
zaynegoering <zaynegoering@gmail.com>
2021-04-01 15:44:49 -0700

    move data to show page

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -7,8 +7,7 @@ function RecipeCard(props)  {
         <div id={'recipe-card-' + props.id}>
             <h2>{props.recipe.name}</h2>
             <Link to={`/recipes/${props.id}`}><img src={props.recipe.img_url} className="recipe-image img-thumbnail img-fluid" alt="None"/></Link>
-            <h4>Category: {props.recipe.category.name}</h4>
-            <p>{props.recipe.description}</p>
+            <br></br><br></br>
         </div>
     )
 }
