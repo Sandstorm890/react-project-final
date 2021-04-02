bf7f9db5759995551a42884c1c4e9c9c2f8c429a
zaynegoering <zaynegoering@gmail.com>
2021-04-01 18:15:43 -0700

    change alt text

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -6,7 +6,7 @@ function RecipeCard(props)  {
     return(
         <div id={'recipe-card-' + props.id}>
             <h2>{props.recipe.name}</h2>
-            <Link to={`/recipes/${props.id}`}><img src={props.recipe.img_url} className="recipe-image img-thumbnail img-fluid" alt="None"/></Link>
+            <Link to={`/recipes/${props.id}`}><img src={props.recipe.img_url} className="recipe-image img-thumbnail img-fluid" alt="Image Not Found"/></Link>
             <br></br><br></br>
         </div>
     )
