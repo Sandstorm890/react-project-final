04f3a594721408c9f0e5f61edef33d741de9dc20
zaynegoering <zaynegoering@gmail.com>
2021-04-02 10:39:06 -0700

    update className list

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
   render() {
     
     return (
-      <div className="App">
+      <div className="App App-background">
         <Router>
           <Header />
           <Switch>
