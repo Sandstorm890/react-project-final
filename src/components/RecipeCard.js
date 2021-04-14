7c9f3709fad3bf00afc224d61b52cf59eece6de7
zaynegoering <zaynegoering@gmail.com>
2021-04-13 19:32:52 -0700

    coinvert recipe card from Rails to MealDB

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom';
 
 
 function RecipeCard(props)  {
-    
+    // console.log(props.id)
     return(
         <div id={'recipe-card-' + props.id}>
-            <h2>{props.recipe.name}</h2>
-            <Link to={`/recipes/${props.id}`}><img src={props.recipe.img_url} className="recipe-image img-thumbnail img-fluid" alt="Image Not Found"/></Link>
+            <h2>{props.name}</h2>
+            <Link to={`/recipes/archive/${props.id}`}><img src={props.image} className="recipe-image img-thumbnail img-fluid" alt="Image Not Found"/></Link>
             <br></br><br></br>
         </div>
     )
