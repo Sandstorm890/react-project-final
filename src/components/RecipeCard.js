5380fed7474575f523f9ee53921cffca7aafe46c
zaynegoering <zaynegoering@gmail.com>
2021-05-18 13:33:53 -0700

    add custom image size

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
-
+import '../App.css'
 
 function RecipeCard(props)  {
     // console.log(props.id)
     return(
         <div id={'recipe-card-' + props.id}>
             <h2>{props.name}</h2>
-            <Link to={`/recipes/archive/${props.id}`}><img src={props.image} className="recipe-image img-thumbnail img-fluid" alt="Image Not Found"/></Link>
+            <Link to={`/recipes/archive/${props.id}`}><img src={props.image} className="recipe-image photo img-thumbnail img-fluid" alt="Image Not Found"/></Link>
             <br></br><br></br>
         </div>
     )
