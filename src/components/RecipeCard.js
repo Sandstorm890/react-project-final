9c19744749375edd4966060727cba69be541a3a4
zaynegoering <zaynegoering@gmail.com>
2021-05-25 15:44:44 -0700

    chnage formatting for grid layout

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -2,11 +2,12 @@ import { Link } from 'react-router-dom';
 import '../App.css'
 
 function RecipeCard(props)  {
-    // console.log(props.id)
+    
     return(
-        <div id={'recipe-card-' + props.id}>
+        <div id={'recipe-card-' + props.id} class='card'>
+            
+            <Link to={`/recipes/archive/${props.id}`}><img src={props.image} className="recipe-image photo img-thumbnail img-fluid" alt="Not Found"/></Link>
             <h2>{props.name}</h2>
-            <Link to={`/recipes/archive/${props.id}`}><img src={props.image} className="recipe-image photo img-thumbnail img-fluid" alt="Image Not Found"/></Link>
             <br></br><br></br>
         </div>
     )
